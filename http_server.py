3c54ba42ede430d3f07031d451f83511d7d9e268
Mulham_Fetna <76975497+molhamfetnah@users.noreply.github.com>
2025-02-24 20:40:18 +0300

    flask http server for the index.html frontend demo file

---
+++ http_server.py
@@ -0,0 +1,16 @@
+# frontend_server.py
+from flask import Flask, send_from_directory
+import os
+
+app = Flask(__name__)
+
+# Path to your HTML file
+FRONTEND_FOLDER = os.path.abspath(os.path.dirname(__file__))
+HTML_FILE = os.path.join(FRONTEND_FOLDER, 'index.html')
+
+@app.route('/')
+def serve_frontend():
+    return send_from_directory(FRONTEND_FOLDER, 'index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)  # Different port from your backend
